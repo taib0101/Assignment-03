a8b89a42f22633733a79a7383e5bcb40aa99a741
taib0101 <taibvai13@gmail.com>
2024-11-04 01:33:41 +0600

    updated

--- controller/controller.js
+++ controller/controller.js
@@ -1,19 +1,19 @@
 export const createBlog = (request, response) => {
     console.log("Blog created successfully");
-    response.status(200).send("Blog created successfully");
+    return response.status(200).send("Blog created successfully");
 };
 
 export const readBlog = (request, response) => {
     console.log("Blog read successfully");
-    response.status(200).send("Blog read successfully");
+    return response.status(200).send("Blog read successfully");
 };
 
 export const updateBlog = (request, response) => {
     console.log("Blog update successfully");
-    response.status(200).send("Blog update successfully");
+    return response.status(200).send("Blog update successfully");
 };
 
 export const deleteBlog = (request, response) => {
     console.log("Blog delete successfully");
-    response.status(200).send("Blog delete successfully");
+    return response.status(200).send("Blog delete successfully");
 };
